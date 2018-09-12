08195cf3ca73af2665ff08fc4ffc84ab49b8aec0
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-12 08:59:27 -0500

    Commit 9 - Created loops.js file

---
+++ loop-questions/app.js
@@ -0,0 +1,50 @@
+console.log("JS is running!");
+
+// // Create a loop that logs the numbers from 0-99, (ascending)
+// for (i = 0; i <= 99; i++){
+// 	console.log(i);
+// }
+
+// // Create a loop that logs the numbers from 99-0 (descending)
+// for (i = 99; i >= 0; i--){
+// 	console.log(i);
+// }
+
+// Create a loop that logs the numbers from 298-1, counting down by 3 (the code in the loop should run 99 times)
+// let j = 0;
+// for (i = 298; i >= 1; i -= 3){
+// 	console.log(`Loop has run ${j} times.`);
+// 	console.log(i);
+// 	j++;
+// }
+
+// Create a loop that logs the EVEN numbers from 0-98 (ascending)
+// for (i = 0; i <= 98; i++){
+// 	if ((i % 2) == 0){
+// 		console.log(i);
+// 	}
+// }
+
+// Create a loop that logs the ODD numbers from 98-0 (descending)
+// for (i = 98; i >= 0; i--){
+// 	if ((i % 2) == 1){
+// 		console.log(i);
+// 	}
+// }
+
+// Create a loop that logs the numbers from 49-72 (ascending)
+// for (i = 49; i <= 72; i++){
+// 	console.log(i);
+// }
+
+// // Create a loop that logs the numbers from 81-26 (descending)
+// for (i = 81; i >= 26; i--){
+// 	console.log(i);
+// }
+
+// // Create a loop that logs the numbers from 1-91 that are multiples of 3 (ascending)
+// for (i = 1; i <= 91; i++){
+// 	if ((i % 3) == 0){
+// 		console.log(i);
+// 	}
+// }
